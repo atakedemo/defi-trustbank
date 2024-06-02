867ac7d2eb21bc98b2b89eb8cb8b8c05801777f0
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-06-02 09:18:28 +0900

    CICD（Github Actions）の動作確認

--- 10_frontend/wallet_webapp/src/app/oid4vci/page.tsx
+++ 10_frontend/wallet_webapp/src/app/oid4vci/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from 'react';
-import { useRouter } from "next/router";
-import { CredentialOfferClient, MetadataClient,} from '@sphereon/oid4vci-client';
 import axios from 'axios';
 import {QrScan} from '@/components/qr-scan'
-import {WalletConnect} from '@/components/wallet-connect'
 
 export default function IssueVc(){
     return (
