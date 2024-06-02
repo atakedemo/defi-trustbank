7eac2b8e3af9117cac1285fbe81c7cd15e03640c
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-06-02 09:53:17 +0900

    CICD（Github Actions）の動作確認02

--- 10_frontend/wallet_webapp/src/app/oid4vp/page.tsx
+++ 10_frontend/wallet_webapp/src/app/oid4vp/page.tsx
@@ -7,7 +7,7 @@ export default function PresentationVP(){
     return (
         <main>
             <h1>Present Your VP</h1>
-            <h2>Select VCs</h2>
+            <h2>Select VCs 1</h2>
         </main>
     )
 }
