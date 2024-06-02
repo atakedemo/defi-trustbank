68cd8785bd0f2ef0263236498bb51f1c8c91badd
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-06-02 09:02:38 +0900

    不要コメントの削除

--- 10_frontend/wallet_webapp/src/app/oid4vci/page.tsx
+++ 10_frontend/wallet_webapp/src/app/oid4vci/page.tsx
@@ -13,29 +13,3 @@ export default function IssueVc(){
         </main>
     )
 }
-
-// const getAccessToken = async(initiationURI_: string) => {
-//     const _initiationRequestWithUrl = await CredentialOfferClient.fromURI(initiationURI_);
-//     const _metadata = await MetadataClient.retrieveAllMetadataFromCredentialOffer(_initiationRequestWithUrl);
-
-//     console.log(_initiationRequestWithUrl.preAuthorizedCode);
-//     console.log(_metadata.token_endpoint);
-//     console.log(_metadata.issuer);
-    
-//     const clientId = '218232426';
-    
-//     const tokenReq = new URLSearchParams();
-//     tokenReq.append('client_id', clientId);
-//     tokenReq.append('grant_type', 'urn:ietf:params:oauth:grant-type:pre-authorized_code');
-//     tokenReq.append('pre-authorized_code', 'aFYv7nBLVxB8n6rjNOGs3MSsAcpQVEWT9JVn886S2M0');
-
-//     await axios.post(_metadata.token_endpoint, tokenReq, {
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         },
-//     }).then(res => {
-//         console.log(res.data)
-//     }).catch(e=> {
-//         console.log(e.response.data);
-//     });
-// }
